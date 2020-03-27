5f842743f2075f261f82831b0049932837e82263
HUSEYNIYON <huseyn19.burhonov@gmail.com>
2020-03-27 19:38:33 +0300

    Text

--- Programm.cs
+++ Programm.cs
@@ -8,11 +8,11 @@ namespace lesson3
 
             decimal userBalance = 300m;
             string basket = "";
-            string erroBalanceMessage = "У вас недостаточно средств";
+            string erroBalanceMessage = "You have'nt any money in you balance!";
 
         start: //Метка "start"
 
-            System.Console.WriteLine($"Ваш баланс - {userBalance}");
+            System.Console.WriteLine($"You balance - {userBalance}");
 
             if (userBalance <= 0)
             {
@@ -21,13 +21,13 @@ namespace lesson3
             };
 
             System.Console.WriteLine("" +
-            "1 Кола - 10 сомон\n" +
-            "2 Сникерс - 7 сомон\n" +
-            "3 Марс - 9 сомон\n" +
-            "4 M&Ms - 4 сомон\n" +
-            "5 Чипсы - 14 сомон\n" +
-            "6 Выход\n" +
-            "Ваш выбор (1-6):");
+            "1 Cola - 10 somoni\n" +
+            "2 Snikers - 7 somoni\n" +
+            "3 Mars - 9 somoni\n" +
+            "4 M&Ms - 4 somoni\n" +
+            "5 Chipsi - 14 somoni\n" +
+            "6 Exit\n" +
+            "Your choice (1-6):");
 
             int choice = int.Parse(Console.ReadLine());
 
@@ -38,7 +38,7 @@ namespace lesson3
                     {
                         if (userBalance - 10 >= 0)
                         {
-                            basket += "Кола,";
+                            basket += "Cola,";
                             userBalance -= 10;
                         }
                         else
@@ -46,18 +46,89 @@ namespace lesson3
                             System.Console.WriteLine(erroBalanceMessage);
                             goto End;
                         }
-                        System.Console.Write("Вы хотите продолжить? д/н:");
+                        System.Console.Write("You want to continue? д/н:");
                         if (Console.ReadLine().ToLower() == "д") goto start;
                         else goto End;
                     }
                     break;
+
+                case 2:
+                    {
+                          if (userBalance - 7 >= 0)
+                        {
+                            basket += "Snikers,";
+                            userBalance -= 7;
+                        }
+                        else
+                        {
+                            System.Console.WriteLine(erroBalanceMessage);
+                            goto End;
+                        }
+                        System.Console.Write("You want to continue? д/н:");
+                        if (Console.ReadLine().ToLower() == "д") goto start;
+                        else goto End;
+                    }
+                    break;
+                         case 3:
+                    {
+                          if (userBalance - 9 >= 0)
+                        {
+                            basket += "Mars,";
+                            userBalance -= 9;
+                        }
+                        else
+                        {
+                            System.Console.WriteLine(erroBalanceMessage);
+                            goto End;
+                        }
+                        System.Console.Write("You want to continue? д/н:");
+                        if (Console.ReadLine().ToLower() == "д") goto start;
+                        else goto End;
+                    }
+                    break;
+                    
+                     case 4:
+                    {
+                          if (userBalance - 4 >= 0)
+                        {
+                            basket += "M&Ms,";
+                            userBalance -= 4;
+                        }
+                        else
+                        {
+                            System.Console.WriteLine(erroBalanceMessage);
+                            goto End;
+                        }
+                        System.Console.Write("You want to continue? д/н:");
+                        if (Console.ReadLine().ToLower() == "д") goto start;
+                        else goto End;
+                    }
+                    break;
+                    
+                     case 5:
+                    {
+                          if (userBalance - 14 >= 0)
+                        {
+                            basket += "Chipsido,";
+                            userBalance -= 14;
+                        }
+                        else
+                        {
+                            System.Console.WriteLine(erroBalanceMessage);
+                            goto End;
+                        }
+                        System.Console.Write("You want to continue? д/н:");
+                        if (Console.ReadLine().ToLower() == "д") goto start;
+                        else goto End;
+                    }
+                
                 case 6: goto End;
                 default: goto start;
                 break;
             }
 
         End:
-            System.Console.WriteLine($"Ваши покупки:{basket}\nВаш баланс:{userBalance}");
+            System.Console.WriteLine($"You product:{basket}\nYou balance:{userBalance}");
             Console.ReadKey();
         }
     }
